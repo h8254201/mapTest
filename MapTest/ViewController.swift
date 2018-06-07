7966a39cebc391491f42e1a46a8c2ee8d8fc0648
larvata_ios <larvata_ios@mini50.local>
2018-06-07 16:10:10 +0800

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by larvata_ios on 2018/6/7.
+//  Copyright © 2018年 tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
